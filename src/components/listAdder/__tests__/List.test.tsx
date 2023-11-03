87941c47ea4810a2cd1521ce30847cb8c58412b4
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2023-11-03 09:43:22 -0500

    Test react components

--- src/components/listAdder/__tests__/List.test.tsx
+++ src/components/listAdder/__tests__/List.test.tsx
@@ -1,14 +1,30 @@
-import { test, expect } from "vitest"
-import { render } from '@testing-library/react'
+import { describe, it, expect } from "vitest"
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/vitest'
 
 import List from "../List"
 
-test("must", () => {
-  // Arrange
-  render(<List />)
-
-  // Act
-
-  // Assert
+describe('test headings', () => {
+  it.skip("should render a heading element level 1 with the text: 'Testing en React con Vitest'", () => {
+    // Arrange
+    render(<List />)
   
+    // Act
+    // const element = screen.getByText(/Testing en React con Vitest/i)
+    const element = screen.getByRole('heading', { level: 1, name: /Testing en React con Vitest/i })
+  
+    // Assert
+    expect(element).toBeInTheDocument()
+  })
+
+  it("should render a heading element level 2 with the text: 'Añadir y eliminar elementos de una lista'", () => {
+    // Arrange
+    render(<List />)
+  
+    // Act
+    const element = screen.getByRole('heading', { level: 2, name: /Añadir y eliminar elementos de una lista/i })
+  
+    // Assert
+    expect(element).toBeInTheDocument()
+  })
 })
